2430ed427cc2e6fecd7fcaec172126fadc4f0da9
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-07-18 18:30:21 -0300

    Section 3: Javascript Line Calculator

---
+++ section03/script.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let shape = document.querySelector(".hexagon");
+  let shapeLength = shape.getTotalLength();
+
+  console.log(shapeLength);
+})
